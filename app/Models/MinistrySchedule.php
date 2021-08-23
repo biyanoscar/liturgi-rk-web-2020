25d3198b791bbc19bd4a73b081382b76399032c4
biyanoscar <biyanoscar@gmail.com>
2021-08-23 20:42:24 +0700

    Model Observer untuk banyak Model

--- app/Models/MinistrySchedule.php
+++ app/Models/MinistrySchedule.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Blameable;
 
 class MinistrySchedule extends Model
 {
     use HasFactory;
+    use Blameable;
     protected $guarded = [];
 
     public function massSchedule()
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -29,6 +29,6 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        MinistrySchedule::observe(MinistryScheduleObserver::class);
+        // MinistrySchedule::observe(MinistryScheduleObserver::class);
     }
 }
