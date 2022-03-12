ff6075420f06f27a81fb559e7799d575d4b0f58b
biyanoscar <biyanoscar@gmail.com>
2022-03-12 15:28:37 +0700

    change home url after login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/mass_schedules';
+    public const HOME = '/sunday_masses';
 
     /**
      * The controller namespace for the application.
